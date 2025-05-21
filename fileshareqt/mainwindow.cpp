a3ba8d2580000e1cfe1fc2a59c18bbec36d05160
Tomas-Deane <deanetomas8@gmail.com>
2025-05-21 13:30:10 +0100

    updated ui with home image and login/signup screen

--- fileshareqt/mainwindow.cpp
+++ fileshareqt/mainwindow.cpp
@@ -1,60 +1,84 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "networkmanager.h"
+#include "authcontroller.h"
+#include "logger.h"
+
+#include <sodium.h>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
     , networkManager(new NetworkManager(this))
+    , authController(new AuthController(networkManager, this))
 {
     ui->setupUi(this);
 
-    // Wire up network signals to our slots
-    connect(networkManager, &NetworkManager::connected,    this, &MainWindow::onConnected);
-    connect(networkManager, &NetworkManager::disconnected, this, &MainWindow::onDisconnected);
-    connect(networkManager, &NetworkManager::messageReceived,
-            this, &MainWindow::onDataReceived);
+    // start with connect/signup disabled until we have TCP
+    ui->signupButton->setEnabled(false);
+    ui->loginButton->setEnabled(false);
 
-    ui->sendButton->setEnabled(false);
+    ui->label->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    ui->label->setMinimumSize(0, 0);
+    ui->label->setScaledContents(true);
+    QPixmap pix(":/nrmc_image.png");
+    ui->label->setPixmap(pix);
+
+    // set gui log text box
+    Logger::initialize(ui->consoleTextEdit);
+
+    // hook up network‐state signals
+    connect(networkManager, &NetworkManager::connected,
+            this, &MainWindow::onServerConnected);
+    connect(networkManager, &NetworkManager::disconnected,
+            this, &MainWindow::onServerDisconnected);
+
+    if (sodium_init() < 0) {
+        Logger::log("sodium_init() failed");
+    } else {
+        Logger::log("sodium initialized");
+    }
+
+    // Auto-connect to server on startup
+    const QString host = "gobbler.info";
+    quint16 port = 3210; // we are using this port 3210 for nrmc
+    Logger::log(QString("Auto-connecting to %1:%2").arg(host).arg(port));
+    networkManager->connectToHost(host, port);
+
+    Logger::log("UI setup complete");
 }
 
 MainWindow::~MainWindow()
 {
+    Logger::log("Application exiting");
     delete ui;
 }
 
-void MainWindow::on_connectButton_clicked()
+void MainWindow::onServerConnected()
 {
-    const QString host = ui->hostLineEdit->text();
-    const quint16 port = static_cast<quint16>(ui->portLineEdit->text().toUInt());
-    ui->logTextEdit->append(QString("Connecting to %1:%2…").arg(host).arg(port));
-    networkManager->connectToHost(host, port);
+    Logger::log("Connected to server");
+    ui->signupButton->setEnabled(true);
+    ui->loginButton->setEnabled(true);
 }
 
-void MainWindow::on_sendButton_clicked()
+void MainWindow::onServerDisconnected()
 {
-    const QString msg = ui->inputLineEdit->text();
-    if (msg.isEmpty())
-        return;
-    networkManager->sendMessage(msg);
-    ui->logTextEdit->append("Me: " + msg);
-    ui->inputLineEdit->clear();
+    Logger::log("Disconnected from server");
+    ui->signupButton->setEnabled(false);
+    ui->loginButton->setEnabled(false);
 }
 
-void MainWindow::onConnected()
+void MainWindow::on_signupButton_clicked()
 {
-    ui->logTextEdit->append("Connected!");
-    ui->sendButton->setEnabled(true);
-    ui->connectButton->setEnabled(false);
+    const QString username = ui->usernameLineEdit->text();
+    const QString password = ui->passwordLineEdit->text();
+    authController->signup(username, password);
 }
 
-void MainWindow::onDisconnected()
+void MainWindow::on_loginButton_clicked()
 {
-    ui->logTextEdit->append("Disconnected.");
-    ui->sendButton->setEnabled(false);
-    ui->connectButton->setEnabled(true);
-}
-
-void MainWindow::onDataReceived(const QString &message)
-{
-    ui->logTextEdit->append("Server: " + message);
+    const QString username = ui->usernameLineEdit->text();
+    const QString password = ui->passwordLineEdit->text();
+    authController->login(username, password);
 }
