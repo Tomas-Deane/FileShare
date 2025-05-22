ea35077af96fe52b044d5bf9fd60d158c9c03376
Dara Heaphy <23369914@studentmail.ul.ie>
2025-05-22 14:44:23 +0100

    updated auth controller for new network manager

--- fileshareqt/authcontroller.h
+++ fileshareqt/authcontroller.h
@@ -12,29 +12,26 @@ class AuthController : public QObject
     Q_OBJECT
 
 public:
-    explicit AuthController(NetworkManager *netMgr, QObject *parent = nullptr);
+    explicit AuthController(QObject *parent = nullptr);
 
-    // Called by MainWindow:
     Q_INVOKABLE void signup(const QString &username, const QString &password);
     Q_INVOKABLE void login(const QString &username, const QString &password);
 
 signals:
-    void signupResult(bool success, const QString &error);
-    void loginResult(bool success, const QString &error);
+    void signupResult(bool success, const QString &message);
+    void loginResult(bool success, const QString &message);
 
 private slots:
-    // network events:
-    void onNetSignupResult(bool success, const QString &error);
-    void onNetLoginChallenge(
+    void onSignupResult(bool success, const QString &message);
+    void onLoginChallenge(
         const QByteArray &nonce,
         const QByteArray &salt,
         int opslimit,
         int memlimit,
-        const QByteArray &encryptedPrivKey,
-        const QByteArray &privKeyNonce
+        const QByteArray &encryptedSK,
+        const QByteArray &skNonce
         );
-    void onNetLoginResult(bool success, const QString &error);
-    void onNetServerMessage(const QString &rawJson);
+    void onLoginResult(bool success, const QString &message);
 
 private:
     NetworkManager *networkManager;
