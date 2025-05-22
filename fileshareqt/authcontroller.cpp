d54f850343853a8cc65f6688f1a23ab6df65ebcf
Dara Heaphy <23369914@studentmail.ul.ie>
2025-05-22 14:44:23 +0100

    updated auth controller for new network manager

--- fileshareqt/authcontroller.cpp
+++ fileshareqt/authcontroller.cpp
@@ -2,24 +2,20 @@
 #include "networkmanager.h"
 #include "crypto_utils.h"
 #include "logger.h"
-
-#include <QJsonObject>
-#include <QJsonDocument>
 #include <sodium.h>
 
-AuthController::AuthController(NetworkManager *netMgr, QObject *parent)
+AuthController::AuthController(QObject *parent)
     : QObject(parent)
-    , networkManager(netMgr)
+    , networkManager(new NetworkManager(this))
 {
-    // connect signals from the network manager to slots in here
     connect(networkManager, &NetworkManager::signupResult,
-            this, &AuthController::onNetSignupResult);
+            this, &AuthController::onSignupResult);
     connect(networkManager, &NetworkManager::loginChallenge,
-            this, &AuthController::onNetLoginChallenge);
+            this, &AuthController::onLoginChallenge);
     connect(networkManager, &NetworkManager::loginResult,
-            this, &AuthController::onNetLoginResult);
-    connect(networkManager, &NetworkManager::serverMessage,
-            this, &AuthController::onNetServerMessage);
+            this, &AuthController::onLoginResult);
+    connect(networkManager, &NetworkManager::networkError,
+            this, [=](const QString &e){ Logger::log("Network error: " + e); });
 }
 
 void AuthController::signup(const QString &username, const QString &password)
@@ -29,53 +25,39 @@ void AuthController::signup(const QString &username, const QString &password)
         emit signupResult(false, "Missing username or password");
         return;
     }
-
     pendingUsername = username;
     pendingPassword = password;
 
-    // 1) generate salt
+    // 1) Generate salt
     QByteArray salt(16, 0);
     randombytes_buf(reinterpret_cast<unsigned char*>(salt.data()), 16);
-    Logger::log(QString("Generated salt: %1").arg(QString(salt.toHex())));
 
-    // 2) derive PDK
+    // 2) Derive PDK
     auto pdk = CryptoUtils::derivePDK(
         password, salt,
         crypto_pwhash_OPSLIMIT_MODERATE,
         crypto_pwhash_MEMLIMIT_MODERATE
         );
-    if (pdk.isEmpty()) {
-        Logger::log("Argon2id key derivation failed");
-        emit signupResult(false, "Key derivation failed");
-        return;
-    }
-    Logger::log(QString("Derived PDK: %1").arg(QString(pdk.toHex())));
 
-    // 3) generate keypair
+    // 3) Generate keypair
     QByteArray pubKey, secKey;
     CryptoUtils::generateKeyPair(pubKey, secKey);
-    Logger::log(QString("Generated keypair: pub=%1 sec=%2")
-                    .arg(QString(pubKey.toHex()), QString(secKey.toHex())));
 
-    // 4) encrypt secret key
+    // 4) Encrypt secret key
     QByteArray skNonce;
     auto encryptedSK = CryptoUtils::encryptSecretKey(secKey, pdk, skNonce);
-    Logger::log(QString("Encrypted SK: %1").arg(QString(encryptedSK.toHex())));
 
-    // 5) build JSON and send
-    QJsonObject req;
-    req["action"]            = "signup";
-    req["username"]          = username;
-    req["salt"]              = QString::fromUtf8(salt.toBase64());
-    req["argon2_opslimit"]   = int(crypto_pwhash_OPSLIMIT_MODERATE);
-    req["argon2_memlimit"]   = int(crypto_pwhash_MEMLIMIT_MODERATE);
-    req["public_key"]        = QString::fromUtf8(pubKey.toBase64());
-    req["encrypted_privkey"] = QString::fromUtf8(encryptedSK.toBase64());
-    req["privkey_nonce"]     = QString::fromUtf8(skNonce.toBase64());
-
-    Logger::log(QString("Sending signup request: %1")
-                    .arg(QString(QJsonDocument(req)
-                                     .toJson(QJsonDocument::Compact))));
+    // 5) Build and send signup request
+    QJsonObject req{
+        { "action", "signup" },
+        { "username", username },
+        { "salt", QString::fromUtf8(salt.toBase64()) },
+        { "argon2_opslimit", int(crypto_pwhash_OPSLIMIT_MODERATE) },
+        { "argon2_memlimit", int(crypto_pwhash_MEMLIMIT_MODERATE) },
+        { "public_key", QString::fromUtf8(pubKey.toBase64()) },
+        { "encrypted_privkey", QString::fromUtf8(encryptedSK.toBase64()) },
+        { "privkey_nonce", QString::fromUtf8(skNonce.toBase64()) }
+    };
     networkManager->signup(req);
 }
 
@@ -86,22 +68,20 @@ void AuthController::login(const QString &username, const QString &password)
         emit loginResult(false, "Missing username or password");
         return;
     }
-
     pendingUsername = username;
     pendingPassword = password;
 
-    Logger::log(QString("Sending login request for user '%1'").arg(username));
     networkManager->login(username);
 }
 
-void AuthController::onNetSignupResult(bool success, const QString &error)
+void AuthController::onSignupResult(bool success, const QString &message)
 {
-    Logger::log(QString("SignupResult: success=%1, error='%2'")
-                    .arg(success).arg(error));
-    emit signupResult(success, error);
+    Logger::log(QString("SignupResult: success=%1, message='%2'")
+                    .arg(success).arg(message));
+    emit signupResult(success, message);
 }
 
-void AuthController::onNetLoginChallenge( // params from server db
+void AuthController::onLoginChallenge(
     const QByteArray &nonce,
     const QByteArray &salt,
     int opslimit,
@@ -110,44 +90,22 @@ void AuthController::onNetLoginChallenge( // params from server db
     const QByteArray &skNonce
     )
 {
-    Logger::log(QString("LoginChallenge received: nonce=%1 salt=%2 ops=%3 mem=%4")
-                    .arg(QString(nonce.toHex()), QString(salt.toHex()))
-                    .arg(opslimit).arg(memlimit));
-
-    // re-derive PDK
+    // 1) Derive PDK from stored password
     auto pdk = CryptoUtils::derivePDK(pendingPassword, salt, opslimit, memlimit);
-    if (pdk.isEmpty()) {
-        Logger::log("Argon2id re-derive failed");
-        networkManager->authenticate(pendingUsername, {});
-        return;
-    }
-    Logger::log(QString("Re-derived PDK: %1").arg(QString(pdk.toHex())));
 
-    // decrypt secret key
+    // 2) Decrypt secret key
     auto secKey = CryptoUtils::decryptSecretKey(encryptedSK, pdk, skNonce);
-    if (secKey.isEmpty()) {
-        Logger::log("Decrypt secretKey failed");
-        networkManager->authenticate(pendingUsername, {});
-        return;
-    }
-    Logger::log(QString("Decrypted secretKey: %1").arg(QString(secKey.toHex())));
 
-    // sign challenge
+    // 3) Sign the challenge
     auto signature = CryptoUtils::signMessage(nonce, secKey);
-    Logger::log(QString("Signature: %1").arg(QString(signature.toHex())));
 
+    // 4) Send authenticate request
     networkManager->authenticate(pendingUsername, signature);
 }
 
-void AuthController::onNetLoginResult(bool success, const QString &error) // for now this class handles logging (to keep networkManager purer). This logic may not belong here
+void AuthController::onLoginResult(bool success, const QString &message)
 {
-    Logger::log(QString("LoginResult: success=%1, error='%2'")
-                    .arg(success).arg(error));
-    emit loginResult(success, error);
-}
-
-void AuthController::onNetServerMessage(const QString &rawJson)
-{
-    // still log every raw line:
-    Logger::log(QString("Server → %1").arg(rawJson));
+    Logger::log(QString("LoginResult: success=%1, message='%2'")
+                    .arg(success).arg(message));
+    emit loginResult(success, message);
 }
