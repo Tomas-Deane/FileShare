ee750a821c54e0a0444f2ee65bf28f9187b3f5cc
DaraHeaphy <23369914@studentmail.ul.ie>
2025-05-27 10:09:27 +0100

    new authcontroller classes with file logic moved to file controller

--- fileshareqt/controllers/authcontroller.h
+++ fileshareqt/controllers/authcontroller.h
@@ -3,7 +3,6 @@
 
 #include <QObject>
 #include <QString>
-#include <QStringList>
 #include <QByteArray>
 
 class NetworkManager;
@@ -19,15 +18,8 @@ public:
     Q_INVOKABLE void login(const QString &username, const QString &password);
     Q_INVOKABLE void logout();
 
-    Q_INVOKABLE void uploadFile(const QString &filename, const QString &fileContents);
-    Q_INVOKABLE void listFiles();
-    Q_INVOKABLE void downloadFile(const QString &filename);
-    Q_INVOKABLE void deleteFile(const QString &filename);
-
-    // Expose ping/check
     Q_INVOKABLE void checkConnection();
 
-    // Accessors for session data (used by ProfileController)
     QString getSessionUsername() const;
     QByteArray getSessionSecretKey() const;
     QByteArray getSessionKek() const;
@@ -40,69 +32,35 @@ signals:
     void loginResult(bool success, const QString &message);
     void loggedIn(const QString &username);
     void loggedOut();
-
-    void uploadFileResult(bool success, const QString &message);
-    void listFilesResult(bool success, const QStringList &files, const QString &message);
-    void downloadFileResult(bool success, const QString &filename, const QByteArray &data, const QString &message);
-    void deleteFileResult(bool success, const QString &message);
-
-    // Forwarded connection status
     void connectionStatusChanged(bool online);
 
 private slots:
-    // Signup/login callbacks
     void onSignupResult(bool success, const QString &message);
-    void onLoginChallenge(
-        const QByteArray &nonce,
-        const QByteArray &salt,
-        int opslimit,
-        int memlimit,
-        const QByteArray &encryptedSK,
-        const QByteArray &skNonce,
-        const QByteArray &encryptedKek,
-        const QByteArray &kekNonce
-        );
+    void onLoginChallenge(const QByteArray &nonce,
+                          const QByteArray &salt,
+                          int opslimit,
+                          int memlimit,
+                          const QByteArray &encryptedSK,
+                          const QByteArray &skNonce,
+                          const QByteArray &encryptedKek,
+                          const QByteArray &kekNonce);
     void onLoginResult(bool success, const QString &message);
-
     void onChallengeReceived(const QByteArray &nonce, const QString &operation);
-
     void onConnectionStatusChanged(bool online);
-
-    void onUploadFileNetwork(bool success, const QString &message);
-    void onListFilesNetwork(bool success, const QStringList &files, const QString &message);
-    void onDownloadFileNetwork(bool success,
-                               const QString &encryptedFileB64,
-                               const QString &fileNonceB64,
-                               const QString &encryptedDekB64,
-                               const QString &dekNonceB64,
-                               const QString &message);
-    void onDeleteFileNetwork(bool success, const QString &message);
 
 private:
     NetworkManager *networkManager;
-
-    // Credentials during the handshake
     QString pendingUsername;
     QString pendingPassword;
-
-    // For file upload
-    QString pendingFileName;
-    QString pendingFileContents;
-
-    // Session state (cleared on logout)
     QString sessionUsername;
     QByteArray sessionSecretKey;
     QByteArray sessionPdk;
     QByteArray sessionKek;
 
-    // For download/delete
-    QString selectedFilename;
-
-    // Challenge processors for other operations
-    void processUploadFile(const QByteArray &nonce);
-    void processListFiles(const QByteArray &nonce);
-    void processDownloadFile(const QByteArray &nonce);
-    void processDeleteFile(const QByteArray &nonce);
+    void processUploadFile(const QByteArray &nonce) = delete;   // now in FileController
+    void processListFiles(const QByteArray &nonce) = delete;
+    void processDownloadFile(const QByteArray &nonce) = delete;
+    void processDeleteFile(const QByteArray &nonce) = delete;
 };
 
 #endif // AUTHCONTROLLER_H
